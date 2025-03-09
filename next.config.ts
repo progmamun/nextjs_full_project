273bf46f7bd142263eed98b29c0d35ce56042dc9
progmamun <mamun333089@gmail.com>
2025-03-09 15:16:35 +0600

    optimized-code

--- next.config.ts
+++ next.config.ts
@@ -4,25 +4,8 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**", // Double asterisk wildcard matches any hostname
+        hostname: "**",
       },
-      // You can keep the specific patterns if you want to maintain them
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-      },
-      {
-        protocol: "https",
-        hostname: "i.ibb.co",
-      },
-      {
-        protocol: "https",
-        hostname: "image.ibb.co",
-      },
-      {
-        protocol: "https",
-        hostname: "*.ibb.co",
-      }
     ],
     deviceSizes: [320, 420, 768, 1024, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
@@ -33,30 +16,19 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/:all*(svg|jpg|png)',
-        locale: false,
+        source: '/:all*(svg|jpg|png|js|css)',
         headers: [
           {
             key: 'Cache-Control',
             value: 'public, max-age=31536000, must-revalidate',
-          }
+          },
         ],
       },
-      {
-        source: '/:all*(js|css)',
-        locale: false,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, must-revalidate',
-          }
-        ],
-      }
-    ]
+    ];
   },
   experimental: {
-    optimizePackageImports: ['@vercel/analytics', '@vercel/speed-insights']
-  }
-}
+    optimizePackageImports: ['@vercel/analytics', '@vercel/speed-insights'],
+  },
+};
 
 export default nextConfig;
